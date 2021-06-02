9d582154797eb4976eea9690556ecbfc51db6d52
zhengpu <zhengpu6688@163.com>
2021-06-02 22:58:35 +0800

    测试commit

--- 014-springboot-mybatis-3/src/main/java/com/zhengpu/Application.java
+++ 014-springboot-mybatis-3/src/main/java/com/zhengpu/Application.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //扫描mapper类，交给spring管理
 @MapperScan(basePackages = "com.zhengpu.mapper")
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
